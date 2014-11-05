667b56ec750f3c564e5dfe9061705a257704ae85
kramer00 <jeffkramer00@gmail.com>
2014-11-05 11:50:04 -0800

    sliderText

--- js/myscript.js
+++ js/myscript.js
@@ -47,7 +47,7 @@ var images = ["http://placehold.it/350x200/1&text=1", "http://placehold.it/350x2
 
 var current_image_index = 0;
 var slider_container;
-var dots_container;
+
 
 function prev_image_index() {
     if (current_image_index < images.length - 1) {
@@ -67,22 +67,17 @@ function next_image_index() {
 
 $(function() {
     slider_container = $('#slider-container');
-    dots_container = $('#slider-dots-container');
 
     for (var i = 0; i < images.length; i++) {
         var row = images[i];
         var class_name = 'slider-img slider-off';
-        var dot_class_name = '';
 
         if (i == 0) {
             class_name = 'slider-img';
-            dot_class_name = 'dot-current';
         }
 
         var img = $('<img>').attr('src', row).addClass(class_name);
         slider_container.append(img);
-        var dot = $('<div>').addClass('dot fa fa-circle').addClass(dot_class_name)
-        dots_container.append(dot);
     };
 
     $('#prev').click(function() {
@@ -90,14 +85,11 @@ $(function() {
         var prev_index = prev_image_index();
         var current_image = $(slider_container.find('img.slider-img')[current_image_index]);
         var prev_image = $(slider_container.find('img.slider-img')[prev_index]);
-        var current_dot = $(dots_container.find('div.dot')[current_image_index]);
-        var prev_dot = $(dots_container.find('div.dot')[prev_index]);
 
         current_image.animate({
             left : "-=380"
         }, 1000, function() {
             current_image.addClass('slider-off').removeClass('current').attr('style', '');
-            current_dot.removeClass('dot-current');
         });
 
         prev_image.addClass('slider-off');
@@ -105,7 +97,6 @@ $(function() {
             left : "0"
         }, 1000, function() {
             prev_image.removeClass('slider-off').removeClass('current').attr('stlyle', '');
-            prev_dot.addClass('dot-current');
         });
 
         current_image_index++;
@@ -119,14 +110,11 @@ $(function() {
         var next_index = next_image_index();
         var current_image = $(slider_container.find('img.slider-img')[current_image_index]);
         var next_image = $(slider_container.find('img.slider-img')[next_index]);
-        var current_dot = $(dots_container.find('div.dot')[current_image_index]);
-        var next_dot = $(dots_container.find('div.dot')[next_index]);
 
         current_image.animate({
             left : "380"
         }, 1000, function() {
             current_image.addClass('slider-off').removeClass('current').attr('style', '');
-            current_dot.removeClass('dot-current');
         });
 
         next_image.addClass('slider-back');
@@ -134,7 +122,6 @@ $(function() {
             left : "0"
         }, 1000, function() {
             next_image.removeClass('slider-off').removeClass('slider-back').removeClass('current').attr('stlyle', '');
-            next_dot.addClass('dot-current');
         });
 
         current_image_index--;
@@ -144,3 +131,101 @@ $(function() {
     });
 });
 // end of home page slider js
+/*
+var images = [{
+    'url' : 'img/meFingerPoint.jpg',
+    'text' : 'That\'s Me, one finger Pointing at you and three pointing back at me.'
+}, {
+    'url' : 'http://placehold.it/350x200/1&text=1',
+    'text' : 'SEND FLOWERS JUST BECAUSE'
+}, {
+    'url' : 'img/3/event1.png',
+    'text' : 'MAKE YOUR EVENT'
+}];
+
+
+var current_image_index = 0;
+var slider_container;
+
+function prev_image_index() {
+    if (current_image_index < images.length - 1) {
+        return current_image_index + 1;
+    } else {
+        return 0;
+    }
+}
+
+function next_image_index() {
+    if (current_image_index > 0) {
+        return current_image_index - 1;
+    } else {
+        return images.length - 1;
+    }
+}
+
+$(function() {
+    slider_container = $('#slider-container');
+
+    for (var i = 0; i < images.length; i++) {
+        var row = images[i];
+        var class_name = 'slider-img slider-off';
+
+        if (i == 0) {
+            class_name = 'slider-img';
+        }
+        var img = $('<img>').attr('src', row['url']);
+        var img_text = $('<p>').html(row['text']);
+        var image_container = $('<div>').addClass(class_name)
+        image_container.append(img).append(img_text);
+        slider_container.append(image_container);
+    };
+
+    $('#prev').click(function() {
+        var prev_index = prev_image_index();
+        var current_image = $(slider_container.find('.slider-img')[current_image_index]);
+        var prev_image = $(slider_container.find('.slider-img')[prev_index]);
+
+        current_image.animate({
+            left : "-=995"
+        }, 1000, function() {
+            current_image.addClass('slider-off').removeClass('current').attr('style', '');
+        });
+        
+        prev_image.addClass('slider-off');
+        prev_image.animate({
+            left : "0"
+        }, 1000, function() {
+            prev_image.removeClass('slider-off').removeClass('current').attr('stlyle', '');
+        });
+        
+        current_image_index++;
+        if (current_image_index == images.length) {
+            current_image_index = 0;
+        }
+    });
+
+    $('#next').click(function() {
+        var next_index = next_image_index();
+        var current_image = $(slider_container.find('.slider-img')[current_image_index]);
+        var next_image = $(slider_container.find('.slider-img')[next_index]);
+
+        current_image.animate({
+            left : "995"
+        }, 1000, function() {
+            current_image.addClass('slider-off').removeClass('current').attr('style', '');
+        });
+        
+        next_image.addClass('slider-back');
+        next_image.animate({
+            left : "0"
+        }, 1000, function() {
+            next_image.removeClass('slider-off').removeClass('slider-back').removeClass('current').attr('stlyle', '');
+        });
+        
+        current_image_index--;
+        if (current_image_index == -1) {
+            current_image_index = images.length - 1;
+        }
+    });
+});
+// end of home page slider js*/
